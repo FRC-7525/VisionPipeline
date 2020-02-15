f75e2a1be18948ef1fae01d2809f99b5523ff83f
Dewan Thenmalai <dewan.thenmalai@gmail.com>
2020-02-15 14:13:33 -0600

    Collapsed line groups into single lines to ease linear algebra

--- Pipeline.py
+++ Pipeline.py
@@ -4,10 +4,10 @@ from collections import defaultdict
 
 
 def intersection(line1, line2):
-    rho1, theta1 = line1[0]
-    rho2, theta2 = line2[0]
+    rho1, theta1 = line1
+    rho2, theta2 = line2
     
-    A = np.array([[np.cos(theta1), np.sin(theta1)], [np.cos(theta2), np.cos(theta2)]])
+    A = np.array([[np.cos(theta1), np.sin(theta1)], [np.cos(theta2), np.sin(theta2)]])
     b = np.array([[rho1], [rho2]])
     x0, y0 = np.linalg.solve(A,b)
     x0, y0 = int(np.round(x0)), int(np.round(y0))
@@ -15,11 +15,9 @@ def intersection(line1, line2):
 
 def segmentedIntersection(lines):
     intersections = []
-    for i, group in enumerate(lines[:-1]):
-        for nextGroup in lines[i+1:]:
-            for line1 in group:
-                for line2 in nextGroup:
-                    intersections.append(intersection(line1, line2))
+    for i in range(len(lines)):
+        for j in range(i+1, len(lines)):
+            intersections.append(intersection(lines[i], lines[j]))
     return intersections
 
 def splitByAngle(lines):
@@ -37,7 +35,41 @@ def splitByAngle(lines):
     for i, line in zip(range(len(lines)), lines):
         segmented[labels[i]].append(line)
     segmented = list(segmented.values())
-    return segmented
+    
+    avgLines = []
+    
+    for i in range(len(segmented)):
+        reshaped = np.reshape(segmented[i], (-1, 2))
+        average = np.average(reshaped, axis=0)
+        avgLines.append(average)
+    
+    return avgLines
+
+def drawLines(lines,frame):
+    for line in lines:
+        
+        subRho = line[0]
+        subTheta = line[1]
+        
+        a = np.cos(subTheta)
+        b = np.sin(subTheta)
+        
+        x0 = a * subRho
+        y0 = b * subRho
+        
+        x1 = int(np.rint(x0 + 1000 * (-1 * b)))
+        y1 = int(np.rint(y0 + 1000 * (a)))
+        x2 = int(np.rint(x0 - 1000 * (-1 * b)))
+        y2 = int(np.rint(y0 - 1000 * (a)))
+        
+        cv2.line(frame, (x1,y1), (x2, y2), (0,0,255), 1)
+
+def drawIntersections(intersections, frame):
+    for x, y in intersections:
+        try:
+            cv2.circle(frame, (x,y), 3, (255,0,0), -1)
+        except Exception as ex:
+            print("Unknown Error: " + str(ex))
 
 
 cap = cv2.VideoCapture("/dev/video0")
@@ -80,37 +112,18 @@ while(1):
     
     try:
         segments = splitByAngle(lines)
-        intersections = segmentedIntersection(segments)
-    except:
-        print("Linear Algebra Error")
+        flattened = [[line[0], np.pi/2] for line in segments]
+        #intersections = segmentedIntersection(flattened)
+    except Exception as ex:
+        print("Linear Algebra Error: " + str(ex))
     
-    if len(intersections) == 0:
-        continue
+    #if len(intersections) == 0:
+        #continue
     
-    for line in lines:
-        
-        subRho = line[0][0]
-        subTheta = line[0][1]
-        
-        a = np.cos(subTheta)
-        b = np.sin(subTheta)
-        
-        x0 = a * subRho
-        y0 = b * subRho
-        
-        x1 = int(np.rint(x0 + 1000 * (-1 * b)))
-        y1 = int(np.rint(y0 + 1000 * (a)))
-        x2 = int(np.rint(x0 - 1000 * (-1 * b)))
-        y2 = int(np.rint(y0 - 1000 * (a)))
-        
-        cv2.line(frame, (x1,y1), (x2, y2), (0,0,255), 1)
+    drawLines(segments,frame)
     
-    for x, y in intersections:
-        try:
-            cv2.circle(frame, (x,y), 2, (255,0,0))
-        except:
-            print("Unknown Error")
-    
+    #drawIntersections(intersections, frame)
+                                                       
     cv2.imshow("Intersections", frame)
     
     k = cv2.waitKey(5) & 0xFF
